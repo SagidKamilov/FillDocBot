1ec54d0e2be7b4e151e85eb092e59e94a1241133
SagidKamilov <89615854074a@gmail.com>
2023-10-12 08:48:04 +0300

    Добавил кнопки удалить файл и удалить все

--- Bot/keyboard/S_keyboard.py
+++ Bot/keyboard/S_keyboard.py
@@ -5,4 +5,6 @@ def start_kb():
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
     but1 = KeyboardButton("НК")
     but2 = KeyboardButton("Помощь")
-    return kb.row(but1, but2)
+    but3 = KeyboardButton("Удалить файл")
+    but4 = KeyboardButton("Удалить все файлы")
+    return kb.row(but1, but2).row(but3, but4)
