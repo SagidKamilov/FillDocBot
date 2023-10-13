8a7b14b849b28de655320fabe1a47aa437226b18
SagidKamilov <89615854074a@gmail.com>
2023-10-14 01:31:02 +0300

    Зарегистрировал хендлер удаления файлов

--- Bot/main.py
+++ Bot/main.py
@@ -1,14 +1,17 @@
+import os
+
 from aiogram.utils import executor
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from Bot import settings
 
-from Bot.handlers import register_contract_start, register_handler_new_contract
+from Bot import settings
+from Bot.handlers import register_contract_start, register_handler_new_contract, register_handler_delete_contract
 
 
 def _on_startup(dp: Dispatcher):
     register_contract_start(dp)
     register_handler_new_contract(dp)
+    register_handler_delete_contract(dp)
 
 
 def main():
