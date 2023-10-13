99ee639123c2e378ca7d15e1e68ff6854803e0c5
SagidKamilov <89615854074a@gmail.com>
2023-10-14 01:30:35 +0300

    Добавил аннотацию типов

--- Bot/keyboard/NC_keyboard.py
+++ Bot/keyboard/NC_keyboard.py
@@ -1,13 +1,14 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-def choice_template():
+
+def choice_template() -> InlineKeyboardMarkup:
     kb = InlineKeyboardMarkup()
     but1 = InlineKeyboardButton(text="Баданина", callback_data='Б')
     but2 = InlineKeyboardButton(text="Макова", callback_data='М')
     return kb.row(but1, but2)
 
 
-def cancel_enter():
+def cancel_enter() -> InlineKeyboardMarkup:
     kb = InlineKeyboardMarkup()
     but1 = InlineKeyboardButton(text="Завершить заполнени", callback_data="stop")
     but2 = InlineKeyboardButton(text="Назад", callback_data="back")
--- Bot/keyboard/S_keyboard.py
+++ Bot/keyboard/S_keyboard.py
@@ -1,10 +1,11 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-def start_kb():
+def start_kb() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
     but1 = KeyboardButton("НК")
     but2 = KeyboardButton("Помощь")
     but3 = KeyboardButton("Удалить файл")
     but4 = KeyboardButton("Удалить все файлы")
-    return kb.row(but1, but2).row(but3, but4)
+    but5 = KeyboardButton("Выбрать контракт")
+    return kb.row(but1, but2).row(but5).row(but3, but4)
