c724fa59f6b736f229e15d38db47b50b7f3e0e51
SagidKamilov <89615854074a@gmail.com>
2023-10-14 01:27:14 +0300

    Обновил файл Клавиатуры для выбора файлов

--- Bot/keyboard/General.py
+++ Bot/keyboard/General.py
@@ -6,15 +6,14 @@ from typing import List, Dict
 
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from file_operations import find_files_name_file
+from file_operations import FindFiles_ShortNameFile
 
 
-def general_kb(list_doc):
+def general_kb(list_doc: List[Dict[str, str]]) -> InlineKeyboardMarkup:
     kb = InlineKeyboardMarkup()
     for elem_list in list_doc:
-        name_file = elem_list.get(find_files_name_file)
-        data_file = elem_list.get(find_files_name_file)
-        print(data_file)
+        name_file = elem_list.get(FindFiles_ShortNameFile)
+        data_file = elem_list.get(FindFiles_ShortNameFile)
         kb.row(InlineKeyboardButton(text=name_file, callback_data=data_file))
     kb.row(InlineKeyboardButton(text="Отмена", callback_data="cancel_del"))
     return kb
