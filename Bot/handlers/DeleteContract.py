8e6520d8588e8ef04d29e13b23366d3ea119adeb
SagidKamilov <89615854074a@gmail.com>
2023-10-14 01:28:04 +0300

    Создал хендлер для удаление файла по выбору из списка

--- Bot/handlers/DeleteContract.py
+++ Bot/handlers/DeleteContract.py
@@ -8,7 +8,7 @@ from Bot.keyboard import general_kb
 # Вызов функций, производящий операции над файлами
 from file_operations import find_files, delete_file, delete_files
 # Вызов констант
-from file_operations import path_to_doc
+from file_operations import path_to_doc, FindFiles_ShortNameFile, FindFiles_NameFile
 
 
 class DeleteContract(StatesGroup):
@@ -17,20 +17,28 @@ class DeleteContract(StatesGroup):
 
 async def delete_contract_step1(message: types.Message):
     list_of_doc: list = find_files()
-    await message.bot.send_message(message.from_user.id, "Выберите договор, который надо удалить:",
-                                   reply_markup=general_kb(list_of_doc))
+    text = "Выберите договор, который надо удалить:"
+    await message.bot.send_message(message.from_user.id, text=text, reply_markup=general_kb(list_of_doc))
     await DeleteContract.step1.set()
 
 
 async def delete_contract_step2(callback: types.CallbackQuery, state: FSMContext):
     if callback.data == "cancel_del":
-        await callback.bot.edit_message_text(text="Удаление файлов остановлено!")
+        text = "Удаление файлов остановлено!"
+        await callback.bot.send_message(callback.from_user.id, text=text)
         await state.finish()
     else:
         list_of_doc: list = find_files()
-        result: str = delete_file(file_name=callback.data[:-1]) + " Хотите еще удалить что-нибудь?"
-        await callback.bot.edit_message_text(text=result, reply_markup= general_kb(list_of_doc))
-        await DeleteContract.previous()
+        result: str
+        for elem in list_of_doc:
+            if elem.get(FindFiles_ShortNameFile) == callback.data:
+                result = delete_file(file_name=elem.get(FindFiles_NameFile))
+                break
+            else:
+                continue
+        await callback.bot.send_message(callback.from_user.id, text=result)
+        text = "Хотите еще удалить что-нибудь?"
+        await callback.bot.send_message(callback.from_user.id, text=text, reply_markup=general_kb(list_of_doc))
 
 
 async def delete_all(message: types.Message):
@@ -42,7 +50,9 @@ def register_handler_delete_contract(dp: Dispatcher):
     dp.register_message_handler(delete_contract_step1, commands=["delete_contract"], state=None)
     dp.register_message_handler(delete_contract_step1, Text(equals="Удалить файл"), state=None)
     dp.register_callback_query_handler(delete_contract_step2,
-                                       lambda message:  message.data == "Заявка" or message.data == "cancel_del",
+                                       lambda message: message.data in [elem.get(FindFiles_ShortNameFile)
+                                                                        for elem in
+                                                                        find_files()] or message.data == "cancel_del",
                                        state=DeleteContract.step1)
     dp.register_message_handler(delete_all, commands=["delete_all"])
     dp.register_message_handler(delete_all, Text(equals="Удалить все файлы"))
