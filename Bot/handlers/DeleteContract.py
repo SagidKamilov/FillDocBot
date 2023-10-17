defc5ef7c0095f6cf5942921bd90a05cba555ade
SagidKamilov <89615854074a@gmail.com>
2023-10-17 20:34:41 +0300

    Исключил повторное появление файлов в списке демонстрации при удалении файлов

--- Bot/handlers/DeleteContract.py
+++ Bot/handlers/DeleteContract.py
@@ -32,8 +32,8 @@ async def cancel(callback: types.CallbackQuery, state: FSMContext):
     await callback.bot.send_message(callback.from_user.id, text=text)
 
 
-async def delete_contract_step2(callback: types.CallbackQuery, state: FSMContext):
-    list_of_doc: list = find_files()
+async def delete_contract_step2(callback: types.CallbackQuery):
+    list_of_doc: list | str = find_files()
     result: str
     for elem in list_of_doc:
         if elem.get(FindFiles_ShortNameFile) == callback.data:
@@ -42,6 +42,7 @@ async def delete_contract_step2(callback: types.CallbackQuery, state: FSMContext
             break
         else:
             continue
+    list_of_doc: list | str = find_files()
     text = "Хотите еще удалить что-нибудь?"
     await callback.bot.send_message(callback.from_user.id, text=text, reply_markup=general_kb(list_of_doc))
 
